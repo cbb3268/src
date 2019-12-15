b39cb7e160ed65c3e3b9049a27d0892ab62ef7c0
nigel.wareham@gmail.com <nigel.wareham@gmail.com>
2019-12-16 10:57:00 +1300

    Added the locations page section

--- Nib.Exercise/Controllers/CareersController.cs
+++ Nib.Exercise/Controllers/CareersController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using Nib.Exercise.Interfaces;
 using Nib.Exercise.Models;
 
 namespace Nib.Exercise.Controllers
@@ -12,10 +15,40 @@ namespace Nib.Exercise.Controllers
 
     public class CareersController : Controller
     {
+        #region MEMBERS
+
+        private readonly ILogger<CareersController> _logger;
+
+        private readonly ILocations _locations;
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public CareersController(ILogger<CareersController> logger
+            , ILocations locations)
+        {
+            _logger = logger;
+            _locations = locations;
+        }
+
+        #endregion
+
 
         public IActionResult Careers()
         {
-            return View();
+            try
+            {
+                var task = CacheLocationList();
+                task.Wait();
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Unknown exception trying to create Careers view ");
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -23,5 +56,27 @@ namespace Nib.Exercise.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        #region HELPERS
+
+        /// <summary>
+        /// Adds the location list data to the ViewData bag
+        /// </summary>
+        /// <returns>The locations</returns>
+        private async Task CacheLocationList()
+        {
+            var locations = await _locations.GetLocations();
+            var locationList = (from n in locations
+                                select new SelectListItem
+                                {
+                                    Text = n.Name,
+                                    Value = n.Id.ToString()
+                                }).ToList();
+
+            _logger.LogDebug($"locations retrieved {locationList.Count}");
+            ViewData["LocationList"] = locationList;
+        }
+
+        #endregion
     }
 }
